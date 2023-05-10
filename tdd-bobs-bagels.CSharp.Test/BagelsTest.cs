f5d2b83d882205c36521e095babe9384676b2fa5
joeri luteyn <joeriluteyn@gmail.com>
2023-05-10 12:23:57 +0200

    first test

---
+++ tdd-bobs-bagels.CSharp.Test/BagelsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace tdd_bobs_bagels.CSharp.Test
+{
+    [TestFixture]
+    public class BagelsTest
+    {
+             
+        [TestCase("regular bagel")]
+        public void AddBagelTest(string bagel)
+        {
+            Bagels bagels = new Bagels();
+            bagels.AddBagel(bagel);
+            Assert.AreEqual(1, bagels.BagelBasket.Count);
+        }
+    }
+}
