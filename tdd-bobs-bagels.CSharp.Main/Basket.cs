2cd614471a359470f599e1a3e30023036cef15fc
StavrosSpil <spiliopoulos.stavros@hotmail.com>
2023-05-10 12:17:35 +0300

    Almost done

---
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Basket
+    {
+        public List<string> bagels = new List<string>();
+        public int BasketCapacity { get; set; } = 3;
+
+        public bool AddBagel(string bagel)
+        {
+            if (bagels.Count() < BasketCapacity)
+            {
+                bagels.Add(bagel);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveBagel(string bagel)
+        {
+            return bagels.Remove(bagel);
+        }
+
+        
+
+
+    }
+}
---
+++ tdd-bobs-bagels.CSharp.Test/BasketTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System.Reflection.Emit;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace tdd_bobs_bagels.CSharp.Test
+{
+    public class BasketTests
+    {
+        //private Basket _basket;
+        public Basket basket;
+        public BasketTests()
+        {
+           //_basket = new Basket();
+        }
+
+        [TestCase("Montreal bagel")]
+        public void TestAddBagel(string bagel)
+        {
+            basket = new Basket();
+            Assert.AreEqual(basket.AddBagel(bagel), true);
+        }
+
+        [TestCase("Egg bagel")]
+        public void TestRemoveBagel(string bagel)
+        {
+            basket = new Basket();
+            basket.AddBagel(bagel);
+
+            bool result = basket.RemoveBagel(bagel);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void TestBasketCapacity()
+        {
+            basket = new Basket();
+            basket.AddBagel("sda");
+            basket.AddBagel("eqweq");
+            basket.AddBagel("xzczx");
+            bool result = basket.AddBagel("dfghdfg");
+            bool expected = false;
+
+            Assert.AreEqual(result, false);
+        }
+
+        [TestCase("Egg bagel", false)]
+        public void TestRemoveBagelIfItDoesntExist(string bagel, bool expected)
+        {
+            basket = new Basket();
+            bool result = basket.RemoveBagel(bagel);
+            Assert.AreEqual(result, expected);
+
+        }
+    }
+}
