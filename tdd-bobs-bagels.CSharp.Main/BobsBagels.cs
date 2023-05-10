dde9f903fffb68e2610c6c5d32667aae37dabdff
Nikita Pieters <nikita_pieters@hotmail.com>
2023-05-10 17:28:24 +0200

    first two userstories done

--- tdd-bobs-bagels.CSharp.Main/BobsBagels.cs
+++ tdd-bobs-bagels.CSharp.Main/BobsBagels.cs
@@ -8,5 +8,15 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class BobsBagels
     {
+        public List <string> basket = new List <string> ();
+        public void AddProduct(string item)
+        {
+            basket.Add(item);
+        }
+
+        public void RemoveProduct(string item)
+        {
+            basket.Remove(item);
+        }
     }
 }
--- tdd-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
+++ tdd-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
@@ -6,15 +6,34 @@ namespace tdd_bobs_bagels.CSharp.Test
     [TestFixture]
     public class BobsBagelsTests
     {
-        private BobsBagels _basket;
-        public BobsBagelsTests()
-        {
-            _basket = new BobsBagels();
-        }
+        //private BobsBagels _basket;
+        //public BobsBagelsTests()
+        //{
+        //    _basket = new BobsBagels();
+        //}
         [Test]
         public void InitialTest()
         {
             Assert.Pass();
+        }
+        [Test]
+        public void TestAddBagel()
+        {
+            //I'd like to add a specific type of bagel to my basket
+            BobsBagels _basket = new BobsBagels();
+            _basket.AddProduct("Cheese bagel");
+            Assert.IsTrue(_basket.basket.Contains("Cheese bagel"));
+        }
+        [Test]
+        public void TestRemoveBagel()
+        {
+            //I'd like to remove a bagel from my basket
+            BobsBagels _basket = new BobsBagels();
+            _basket.AddProduct("Cheese bagel");
+            _basket.AddProduct("Salmon bagel");
+            _basket.RemoveProduct("Salmon bagel");
+            Assert.IsFalse(_basket.basket.Contains("Salmon bagel"));
+
         }
     }
 }
