5acf43a37713a5296e6ee3eedfc8dbafc24e7132
Max de Ruiter <m.j.deruiter99@hotmail.com>
2023-05-10 15:14:43 +0200

    Standard

--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -8,5 +8,48 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Core
     {
+        public List<string> basket = new List<string>();
+        
+        public List<string> addBagel(string bagel)
+        {
+            if (basket.Contains(bagel))
+            {
+                Console.WriteLine($"{bagel} is already in basket.");
+            } else
+            {
+                if (basket.Count < basketMax)
+                {
+                    basket.Add(bagel);
+                }
+                else
+                {
+                    Console.WriteLine("Basket is full");
+                }
+            }
+            return basket;
+        }
+
+        public bool removeBagel(string bagel)
+        {
+            if (basket.Contains(bagel))
+            {
+                basket.Remove(bagel);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{bagel} is not in basket.");
+                return false;
+            }
+        }
+
+        public int basketMax { get; set; } = 3;
+        public bool basketFull { get { return basketMax >= basket.Count ? true : false; } }
+        public void changeCapacity(int max)
+        {
+            basketMax = max;
+
+            basket.RemoveRange(max, basket.Count - max);
+        }
     }
 }
--- tdd-bobs-bagels.CSharp.Test/CoreTests.cs
+++ tdd-bobs-bagels.CSharp.Test/CoreTests.cs
@@ -1,4 +1,6 @@
-﻿using tdd_bobs_bagels.CSharp.Main;
+﻿using NUnit.Framework;
+using System.IO;
+using tdd_bobs_bagels.CSharp.Main;
 
 namespace tdd_bobs_bagels.CSharp.Test
 {
@@ -8,6 +10,98 @@ namespace tdd_bobs_bagels.CSharp.Test
         public CoreTests()
         {
             _core = new Core();
+        }
+
+        [Test]
+        public void AddBagelToBasket()
+        {
+            // I'd like to add a specific type of bagel to my basket.
+
+            // arrange
+            string bagel = "Bagel with salmon";
+
+            // act
+            _core.addBagel(bagel);
+
+            // assert
+            Assert.AreEqual(bagel, _core.basket.ElementAt(0));
+        }
+
+        [Test]
+        public void RemoveBagelFromBasket()
+        {
+            // I'd like to remove a bagel from my basket.
+
+            // arrange
+            string bagel = "Bagel with salmon";
+            _core.addBagel(bagel);
+
+            // act
+            _core.removeBagel(bagel);
+
+            // assert
+            Assert.IsFalse(_core.basket.Contains(bagel));
+        }
+
+        [Test]
+        public void CheckIfBasketFull()
+        {
+            // I'd like to know when my basket is full when I try adding an item beyond my basket capacity.
+
+            // arrange
+            string bagel = "Bagel with salmon";
+            string bagel1 = "Bagel with chicken";
+            string bagel2 = "Bagel with beef";
+            string bagel3 = "Bagel with prawns";
+
+            // act
+            _core.addBagel(bagel);
+            _core.addBagel(bagel1);
+            _core.addBagel(bagel2);
+            _core.addBagel(bagel3);
+
+            // assert
+            Assert.AreEqual(_core.basket.Count, _core.basketMax);
+        }
+
+        [TestCase(2)]
+        public void ChangeBasketCapacity(int max)
+        {
+            // I’d like to change the capacity of baskets.
+
+            // arrange
+            string bagel = "Bagel with salmon";
+            string bagel1 = "Bagel with chicken";
+            string bagel2 = "Bagel with beef";
+            string bagel3 = "Bagel with prawns";
+
+            _core.addBagel(bagel);
+            _core.addBagel(bagel1);
+            _core.addBagel(bagel2);
+            _core.addBagel(bagel3);
+
+            // act
+            _core.changeCapacity(max);
+
+            // assert
+            Assert.AreEqual(_core.basket.Count, max);
+        }
+
+        [Test]
+        public void BagelNotInBasket()
+        {
+            // I'd like to know if I try to remove an item that doesn't exist in my basket.
+
+            // arrange
+            string bagel = "Bagel with salmon";
+            string bagel1 = "Bagel with chicken";
+            _core.addBagel(bagel);
+
+            // act
+            _core.removeBagel(bagel1);
+
+            // assert
+            Assert.IsFalse(_core.removeBagel(bagel1));
         }
     }
 }
