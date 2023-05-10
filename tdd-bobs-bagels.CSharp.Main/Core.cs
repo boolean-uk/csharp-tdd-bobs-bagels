b5bcf75ef333f316b7d30ed314494cd42ce9d3a0
Thanasis Andritsios <thanasis1997abc@gmail.com>
2023-05-10 18:43:46 +0300

    added 1 method to add bagel and test if the bagel added succesfuly

--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -8,5 +8,37 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Core
     {
+        private List<string> _basket;
+
+        public Core()
+        {
+            _basket = new List<string>();
+        }
+
+        public int MaxCapacity { get; set; } = 3;
+
+        public int BasketCapacity { get { return _basket.Count; } } 
+
+        public bool AddBagel(string bagel)
+        {
+            if(_basket.Contains(bagel)) { 
+                return false;
+            }else if(BasketCapacity >= MaxCapacity)
+            {
+                return false;
+            }
+            else
+            {
+                _basket.Add(bagel);
+                return true;
+            }
+        }
+
+        public List<string> Basket {
+            get {
+                return _basket;
+          }
+        }
+
     }
 }
--- tdd-bobs-bagels.CSharp.Test/CoreTests.cs
+++ tdd-bobs-bagels.CSharp.Test/CoreTests.cs
@@ -1,13 +1,25 @@
-﻿using tdd_bobs_bagels.CSharp.Main;
+﻿using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
 
 namespace tdd_bobs_bagels.CSharp.Test
 {
+    [TestFixture]   
+    
     public class CoreTests
     {
-        private Core _core;
         public CoreTests()
         {
-            _core = new Core();
+        }
+
+        [Test]
+        public void IsBagelAdded() {
+           Core core = new Core();
+            core.Basket.Add("bagel onion");
+            bool result = core.AddBagel("bagel onion");
+
+            Assert.IsTrue(result);
+
+
         }
     }
 }
