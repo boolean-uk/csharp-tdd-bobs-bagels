74e7773cd339f07e6fbbc041d61d97877d0e05bc
Nikita Pieters <nikita_pieters@hotmail.com>
2023-05-10 16:46:14 +0200

    changed core to BobsBagels

---
+++ tdd-bobs-bagels.CSharp.Test/BobsBagelsTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace tdd_bobs_bagels.CSharp.Test
+{
+    [TestFixture]
+    public class BobsBagelsTests
+    {
+        //private BobsBagels _basket;
+        public BobsBagelsTests()
+        {
+            //_basket = new BobsBagels();
+        }
+        [Test]
+        public void InitialTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
