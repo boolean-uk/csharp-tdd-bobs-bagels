10aa1eb5e78801bc91c19db91d3022d50f3cdb82
Ävadakedavr0 <dylandd4@hotmail.be>
2023-09-12 12:14:24 +0200

    write failing test

--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -8,5 +8,26 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Core
     {
+        public List<string> items = new List<string>();
+        public int capacity = 5; 
+
+        public bool AddBagel(string bagelType)
+        {
+            return false; 
+        }
+
+        public bool RemoveBagel(string bagelType)
+        {
+            return false;
+        }
+
+        public bool IsFull()
+        {
+            return false;
+        }
+
+        public void SetCapacity(int newCapacity)
+        {
+        }
     }
 }
--- tdd-bobs-bagels.CSharp.Test/CoreTests.cs
+++ tdd-bobs-bagels.CSharp.Test/CoreTests.cs
@@ -1,13 +1,49 @@
 ﻿using tdd_bobs_bagels.CSharp.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace tdd_bobs_bagels.CSharp.Test
 {
     public class CoreTests
     {
         private Core _core;
-        public CoreTests()
+
+        [SetUp]
+        public void Setup()
         {
             _core = new Core();
+        }
+
+        [Test]
+        public void CanAddBagel()
+        {
+            bool succes = _core.AddBagel("AvadakedavrO");
+                Assert.IsTrue(succes);
+        }
+
+        [Test]
+        public void CanRemoveBagel()
+        {
+            _core.AddBagel("AvadakedavrO");
+            bool removed = _core.RemoveBagel("AvadakedavrO");
+            Assert.IsTrue(removed);
+        }
+
+        [Test]
+        public void BasketIsFull()
+        {
+            _core.SetCapacity(5);
+            _core.AddBagel("Harry");
+            _core.AddBagel("potter");
+            _core.AddBagel("Lord");
+            _core.AddBagel("Voldemort");
+            _core.AddBagel("Dementor");
+            bool isFull = _core.IsFull();
+            Assert.IsTrue(isFull);
         }
     }
 }
