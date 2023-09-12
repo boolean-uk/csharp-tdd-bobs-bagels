78d1f14e8c44fa1b882a48422e82c69c92d183a1
ks46 <konastaf46@gmail.com>
2023-09-12 16:27:58 +0300

    test for Add4BagelsToBasketTest

--- tdd-bobs-bagels.CSharp.Test/CoreTests.cs
+++ tdd-bobs-bagels.CSharp.Test/CoreTests.cs
@@ -12,5 +12,15 @@ namespace tdd_bobs_bagels.CSharp.Test
             Basket basket = new Basket();
             Assert.IsTrue(basket.Add("plain bagel"));
         }
+
+        [Test]
+        public void Add4BagelsToBasketTest()
+        {
+            Basket basket = new Basket();
+            Assert.IsTrue(basket.Add("plain bagel"));
+            Assert.IsTrue(basket.Add("everything bagel"));
+            Assert.IsTrue(basket.Add("chocolate chip bagel"));
+            Assert.IsFalse(basket.Add("salt bagel"));
+        }
     }
 }
