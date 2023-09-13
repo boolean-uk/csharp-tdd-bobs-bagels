4faac69fb1afbe1e5d29f3e89e3cade909888836
Max Kroon <maxkroon.18@gmail.com>
2023-09-13 17:15:09 +0200

    Core

---
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Basket
+    {
+        public List<string> Items { get; set; } = new List<string>();
+        //public Dictionary<string, bool> Items { get; set; } = new Dictionary<string, bool>();
+        public int Capacity { get; set; }
+
+        public Basket(int capacity)
+        {
+            Capacity = capacity;
+            Items = new List<string>();
+        }
+
+        public bool AddAnotherBagel(string bagel)
+        {
+            if (Items.Count +1 <= Capacity)
+            {                
+                Items.Add(bagel);
+                return true;
+            }
+            return false;
+        }
+
+
+        public bool AddBagel(string bagel )
+        {
+            Items.Add( bagel );
+            return true; 
+            //foreach (var item in Items )
+            //{
+            //    if (item == basket)
+            //    {
+            //        Items[basket] = true;
+            //        return true;
+            //    }
+            //}
+            //return false;
+        }
+
+        public string RemoveBagel( string remove )
+        {
+            foreach (var bagel in Items)
+            {
+                if (bagel == remove)
+                    Items.Remove(bagel);
+                return "Item removed";
+            }
+            return "Are you insane?!";
+        }
+
+        public string SearchBagelByName(string search)
+        {
+            if (Items.Any(x => x == search))
+            {
+                Items.Remove(search);
+                return "Item removed";
+            }
+            return "Are you insane?!";
+
+
+
+            //foreach (var bagel in Items)
+            //{
+            //    if (bagel == search)
+            //    Items.Remove(bagel);
+            //    return "Item removed";
+            //}
+        }
+        
+    }
+}
--- tdd-bobs-bagels.CSharp.Test/CoreTests.cs
+++ tdd-bobs-bagels.CSharp.Test/CoreTests.cs
@@ -1,13 +1,115 @@
-﻿using tdd_bobs_bagels.CSharp.Main;
+﻿using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
 
 namespace tdd_bobs_bagels.CSharp.Test
 {
+    [TestFixture]
     public class CoreTests
     {
-        private Core _core;
-        public CoreTests()
+        [Test]
+       public void AddBagel()
         {
-            _core = new Core();
+            // arrange
+
+            Basket bagel = new Basket(3);
+                
+
+
+            // act
+            bagel.Items.Add("Cheese");
+
+
+            // assert
+            Assert.IsTrue(1 == bagel.Items.Count);
+
+        }
+
+        [Test]
+        public void RemoveBagel() 
+        {
+            // arrange
+            Basket bagel = new Basket(3);
+            bagel.Items.Add("Cheese");
+            bagel.Items.Add("Ham");
+            bagel.Items.Add("Peanut Butter");
+
+            // act
+
+            bagel.RemoveBagel("Ham");
+
+            // assert
+            Assert.IsTrue(2 == bagel.Items.Count);
+
+        }
+
+        //[Test]
+        //public void MaximumBagel()
+        //{
+        //    // arrange
+        //    Basket bagel = new Basket(3);
+        //    bagel.Items.Add("Cheese");
+        //    bagel.Items.Add("Ham");
+        //    bagel.Items.Add("Peanut Butter");
+
+        //    // act
+        //    bagel.Items.Add("Mozzarella");
+        //    bagel.Items.Add("Mozzarella");
+        //    bagel.Items.Add("Mozzarella");
+
+
+        //    // assert
+        //    Assert.IsTrue (bagel.Items.Count <= 3);
+
+        
+
+        [Test]
+        public void MaximumBagelTwo()
+        {
+            Basket bagel = new Basket(4 );
+            bagel.AddAnotherBagel("Cheese");
+            bagel.AddAnotherBagel("Blood");
+            bagel.AddAnotherBagel("Sweat");
+            bagel.AddAnotherBagel("Tears");
+            //bagel.AddAnotherBagel("Tears");
+
+            bool result = bagel.AddAnotherBagel("Ham");
+
+            Assert.IsFalse(result);
+
+        }
+
+        [Test]
+        public void IncreaseMaximumBagel()
+        {
+            // arrange
+            Basket bagel = new Basket(5);
+            bagel.AddAnotherBagel("Cheese");
+            bagel.AddAnotherBagel("Blood");
+            bagel.AddAnotherBagel("Sweat");
+            bagel.AddAnotherBagel("Tears");
+
+            bool result = bagel.AddAnotherBagel("Ham");
+
+            Assert.IsTrue(result);
+
+        }
+
+        [Test]
+        public void SearchBagels()
+        {
+            // arange
+            Basket bagel = new Basket(3);
+            bagel.Items.Add("Cheese");
+            bagel.Items.Add("Ham");
+            bagel.Items.Add("Peanut Butter");
+            bagel.Items.Remove("Bacon");
+
+            // act
+            string result = bagel.SearchBagelByName("Bacon");
+
+            // assert
+            Assert.AreEqual(result, "Are you insane?!");
+
         }
     }
 }
