57f8dac9d3338b3a7f34825e6ca27871771cedb5
Kanthee Kosolyuthasarn <Kanthee>
2024-01-15 13:21:07 +0100

    Kanthee Kosolyuthasarn - C#3 - 15.01.2024: commit 2.1

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -30,4 +30,30 @@ public class Tests
         Assert.AreEqual(testResult, basket._basket);
 
     }
+
+    [Test]
+    public void testRemove()
+    {
+        //Arrange
+        Basket basket = new Basket();
+
+
+        //Act
+        string bagel1 = "bread";
+        string bagel2 = "crossiant";
+        string bagel3 = "Moji";
+        basket.Add(bagel1);
+        basket.Add(bagel2);
+        basket.Add(bagel3);
+
+        basket.Remove(bagel1);
+        List<string> testResult = new List<string>() {
+            bagel2, bagel3
+        };
+
+
+        //Assert
+        Assert.AreEqual(testResult, basket._basket);
+
+    }
 }
