61f9f310c1e0f1462ce11d791e4a41aedaee7e50
llllllll-l <vincent.werelius@gmail.com>
2024-01-15 13:27:50 +0100

    break

---
+++ csharp-tdd-bobs-bagels.tests/BasketTest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace csharp_tdd_bobs_bagels.tests {
+
+    [TestFixture]
+    public class BasketTest {
+        
+
+
+        [SetUp]
+        public void SetUp() {
+            
+        }
+    }
+}
