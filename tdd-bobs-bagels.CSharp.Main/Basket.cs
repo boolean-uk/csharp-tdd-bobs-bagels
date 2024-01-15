9114022b7029844c7b7c65e59b69c802168e6481
Victor <victor.adamson@se.experis.com>
2024-01-15 10:32:09 +0100

    Added test for addBagel()

---
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Basket
+    {
+        public int _basketSize;
+        public List<string> content;
+        public Basket(int basketSize)
+        {
+            _basketSize = basketSize;
+            content = new List<string>();
+        }
+        public bool addBagel(string type)
+        {
+            return false;
+        }
+        public bool removeBagel(string type)
+        {
+            return false;
+        }
+        public int changeCapacity(int currentSize)
+        {
+            return 0;
+        }
+    }
+
+}
---
+++ tdd-bobs-bagels.CSharp.Test/BasketTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace tdd_bobs_bagels.CSharp.Test
+{
+    [TestFixture]
+    public class BasketTests
+    {
+        private Basket _basket;
+        public BasketTests()
+        {
+            _basket = new Basket(6);
+            _basket.addBagel("poppy seed");
+            _basket.addBagel("cream cheese");
+        }
+        [Test]
+        public void addBagelTest()
+        {
+            Assert.IsTrue(_basket.content.Contains("poppy seed"));
+            Assert.IsTrue(_basket.content.Contains("cream cheese"));
+            Assert.IsFalse(_basket.content.Contains("sesame seed"));
+        }
+        [Test]
+        public void removeBagelTest()
+        {
+
+        }
+        [Test]
+        public void changeCapacityTest()
+        {
+
+        }
+    }
+}
