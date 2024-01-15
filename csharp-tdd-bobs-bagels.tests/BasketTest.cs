2ba2107870d5f37e3fd4950248e1ba6c85f4023e
Louis <louisshrestha0412@outlook.com>
2024-01-15 13:29:48 +0100

    first testcase

---
+++ csharp-tdd-bobs-bagels.tests/BasketTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace csharp_tdd_bobs_bagels.tests
+{
+    [TestFixture]
+    internal class BasketTest
+    {
+
+        [Test]
+        public void add() 
+        {
+            // arrange 
+            Basket core = new Basket();
+            string bagel = "cream bagel";
+
+            // act
+            core.basket.Add(bagel);
+            string result = core.basket.first();
+            int length = core.basket.count();
+
+            // assert
+            Assert.AreEqual(bagel, result);
+            Assert.IsTrue(length == 1);            
+        }
+    }
+}
