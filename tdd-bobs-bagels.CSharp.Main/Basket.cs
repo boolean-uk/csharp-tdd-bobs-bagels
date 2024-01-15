edf06c7e46da1fd058933c300a30f570f52011f8
Linus Willmont <linus.willmont@se.experis.com>
2024-01-15 16:35:35 +0100

    Code done

--- tdd-bobs-bagels.CSharp.Main/Basket.cs
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -9,21 +9,39 @@ namespace tdd_bobs_bagels.CSharp.Main
     public class Basket
     {
         public readonly string[] bagelTypes = { "plain", "sesameSeed", "blueberry", "jalapeno" };
-        private int _maxCapacity = 5;
+        private int _maxCapacity = 4;
         public readonly List<string> _basketList = new List<string>();
 
         public string Add(string bagelType)
         {
-            throw new NotImplementedException();
-        }
-        public bool Remove(string bagelType)
-        {
-            throw new NotImplementedException();
+            if (_basketList.Count >= _maxCapacity) return "Basket is full";
+            else if (Array.IndexOf(bagelTypes, bagelType) != -1){
+                _basketList.Add(bagelType);
+                return $"{bagelType} was added to basket";
+            } 
+            else{
+                return $"{bagelType} is not a valid bagle type";
+            }
             
         }
+
+        public bool Remove(string bagelType)
+        {
+            bool wasItemRemoved = _basketList.Remove(bagelType);
+            return wasItemRemoved;
+        }
+
         public bool ChangeCapacity(int newCapacity)
         {
-            throw new NotImplementedException();
+            if(newCapacity < 1 ||  newCapacity < _basketList.Count -1)
+            {
+                return false;
+            }
+            else
+            {
+                _maxCapacity = newCapacity;
+                return true;
+            }
         }
     }
 }
