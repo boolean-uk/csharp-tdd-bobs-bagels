3f415fe067afa2f55d0ffcb5b2785103582402f2
Mikolaj <mkmbaran@gmail.com>
2024-01-15 15:52:12 +0100

    Initialized domain-model and test functions I will be using

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace csharp_tdd_bobs_bagels.tests;
 
 public class Tests
@@ -8,8 +10,53 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void AddTest()
     {
+        //arrange
+
+        //act
+
+        //assert
+        Assert.Pass();
+    }
+    [Test]
+    public void RemoveTest()
+    {
+        //arrange
+
+        //act
+
+        //assert
+        Assert.Pass();
+    }
+    [Test]
+    public void FullBasketTest()
+    {
+        //arrange
+
+        //act
+
+        //assert
+        Assert.Pass();
+    }
+    [Test]
+    public void ExpandBasketTest()
+    {
+        //arrange
+
+        //act
+
+        //assert
+        Assert.Pass();
+    }
+    [Test]
+    public void BagelExistsTest()
+    {
+        //arrange
+
+        //act
+
+        //assert
         Assert.Pass();
     }
 }
