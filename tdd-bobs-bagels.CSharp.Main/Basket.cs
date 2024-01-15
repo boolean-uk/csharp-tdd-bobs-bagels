faa2e3d5f843197a470d7b6c80e7bef8f635f8db
Klara Andersson <klara.andersson@se.experis.com>
2024-01-15 09:39:02 +0100

    Red tests now

---
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Basket
+    {
+        public List<string> basket;
+        public int capacity;
+
+        public Basket()
+        {
+            basket = new List<string>();
+            capacity = 3;
+        }
+
+        public bool AddBagel(string bagelType)
+        {
+            return false;
+        }
+
+        public bool RemoveBagel(string bagelType)
+        {
+            return false;
+        }
+
+        public bool BasketFull()
+        {
+            return false;
+        }
+
+        public int IncreaseCapacity()
+        {
+            return 0;
+        }
+
+        public bool ItemEsists(string bagelType)
+        {
+            return false;
+        }
+    }
+}
---
+++ tdd-bobs-bagels.CSharp.Test/BasketTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace tdd_bobs_bagels.CSharp.Test
+{
+    public class BasketTests
+    {
+        private Basket _core;
+        public BasketTests()
+        {
+            _core = new Basket();
+            _core.AddBagel("Cream");
+            _core.AddBagel("Cheese");
+            _core.AddBagel("Dressing");
+        }
+
+        [Test]
+        public void AddedBagel()
+        {
+            bool haveAdded = _core.basket.Count() > 0;
+            Assert.IsTrue(haveAdded);
+        }
+
+        [Test]
+        public void RemovedBagel()
+        {
+            int beforeRem = _core.basket.Count();
+            _core.RemoveBagel("Cream");
+            int afterRem = _core.basket.Count();
+            Assert.That(beforeRem, Is.GreaterThan(afterRem));
+        }
+
+        [Test]
+        public void IsBasketFull()
+        {
+            bool full = _core.AddBagel("Peanuts");
+            Assert.That(false, Is.EqualTo(full));
+        }
+
+        [Test]
+        public void IncreasedCapacity()
+        {
+            _core.capacity = 6;
+            bool full = _core.AddBagel("Peanuts");
+            Assert.That(true, Is.EqualTo(full));
+        }
+
+        [TestCase("Cream", true)]
+        [TestCase("Jam", false)]
+        [TestCase("Dressing", true)]
+        public void RemovedBagel(string bagelType, bool expected)
+        {
+            bool removed = _core.RemoveBagel(bagelType);
+            Assert.That(expected, Is.EqualTo(removed));
+        }
+    }
+}
