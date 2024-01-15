cf23cfd953d4e0cd5c33c8f14355a4b195c1e129
Kristian <kristian.hjelmtveit@no.experis.com>
2024-01-15 15:20:33 +0100

    Added test for Add()

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -1,15 +1,22 @@
+using System.Reflection.Emit;
+using tdd_bobs_bagels.CSharp.Main;
+
 namespace csharp_tdd_bobs_bagels.tests;
 
+[TestFixture]
 public class Tests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void ShouldAddBagel()
     {
-        Assert.Pass();
+        Core core = new Core();
+        Bagel bagel1 = Bagel.appleCinamon;
+        Bagel bagel2 = Bagel.garlic;
+
+        core.Add(bagel1);
+        core.Add(bagel2);
+        Assert.That(core.Basket.Count > 0);
+        Assert.That(core.Basket.Contains(bagel2));
+        Assert.That(core.Basket.Contains(bagel1));
     }
 }
--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -8,5 +8,17 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Core
     {
+        private List<Bagel> _basket = new List<Bagel>();
+        public List<Bagel> Basket { get {  return _basket; } }
+        public void Add(Bagel bagel1)
+        {
+            throw new NotImplementedException();
+        }
     }
+}
+
+public enum Bagel
+{
+    appleCinamon, blueberryChocolateChip, Egg, cinamonRaisin, everything,
+    frenchToast, garlic, jalapeno, marbleOnion, poppy, plain, pumpernickle
 }
