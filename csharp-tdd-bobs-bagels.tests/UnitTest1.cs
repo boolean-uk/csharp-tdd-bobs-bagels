1771803cdbd0cdd8cb5e0d775426079f1c6023da
oysteinbjo <oystein.bjorkeng@no.experis.com>
2024-01-15 14:05:32 +0100

    add test for add-method exception handling

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -19,4 +19,25 @@ public class Tests
         Assert.That(basket.Bagels.Count != 0);
     }
 
+    [Test]
+    public void TestAddIfBasketIsFull() 
+    {
+        Basket basket = new Basket();
+        Bagel bagel1 = new Bagel("Everything");
+        Bagel bagel2 = new Bagel("Everything");
+        Bagel bagel3 = new Bagel("Everything");
+        Bagel bagel4 = new Bagel("Everything");
+        Bagel bagel5 = new Bagel("Everything");
+
+        basket.AddToBasket(bagel1);
+        basket.AddToBasket(bagel2);
+        basket.AddToBasket(bagel3);
+        basket.AddToBasket(bagel4);
+
+
+        var ex = Assert.Throws<Exception>(() => basket.AddToBasket(bagel5));
+        Assert.Equals(ex.Message, "No more room in basket");
+
+    }
+
 }
