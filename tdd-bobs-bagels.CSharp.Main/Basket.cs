d0c453487a9dfa0599edd73cff865884c481e4bf
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-15 14:56:11 +0100

    Basket scaffolding methods added

--- tdd-bobs-bagels.CSharp.Main/Basket.cs
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -8,5 +8,33 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Basket
     {
+        List<string> _basket;
+        int _maxBasketSize;
+
+        public Basket() 
+        {
+            _basket = new List<string>();
+            _maxBasketSize = 4;
+        }
+
+        public bool AddBagelToBasket(string bagelType) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveBagelFromBasket(string bagelType) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BasketHasRoom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SetBasketSize(int newSize) 
+        {
+            throw new NotImplementedException();
+        }
     }
 }
