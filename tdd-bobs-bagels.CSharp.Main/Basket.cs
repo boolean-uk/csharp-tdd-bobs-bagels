e0cb18915af3c6edadd28b4c67df04e2ad7ab1e7
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-01-15 14:04:12 +0100

    finished tests

---
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Basket
+    {
+        //set basket capacity, i.e list length
+        List<string> basket = new List<string>();
+
+        public Basket() 
+        { 
+            basket = new List<string>(); 
+        }
+
+
+        public bool addBagel(string bagelType)
+        {
+            return true;
+        }
+
+        public bool removeBagel(string bagelType)
+        {
+            return false;
+        }
+
+
+        public int changeCapacity(int newCapacity)
+        {
+            return 0;        
+        }
+    }
+}
+
+
+
+/*
+ - class Basket()
+  
+ 
+ - Properties
+public List<string> basket
+public int basketCapacity
+
+
+-Methods
+bool add bageltype (string)
+        adds a bagel to the basket, outputs true if bagel added and false otherwise
+        If the basket is full, outputs a message saying the basket is full
+
+bool remove bageltype (string)
+        removes a bagel from the basket, outputs true if the bagel is removed and false otherwise
+        If the item is not in the basket, outputs a message saying the item doesn't exist
+
+int changeCapacity(int)
+        changes the capacity of the basket, outputs the new integer capacity
+ 
+ */
---
+++ tdd-bobs-bagels.CSharp.Test/BasketTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace tdd_bobs_bagels.CSharp.Test
+{
+    public class BasketTests
+    {
+        private Basket _basket;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _basket = new Basket();
+        }
+
+        [Test]
+        public void addBageltest()
+        {
+            //testing if the bagel is being addded
+            bool result = _basket.addBagel("Plain");
+            Assert.That(result, Is.EqualTo(true));
+
+            List<string> expected = new List<string> {"Plain"};
+
+            //Assert if list contains "Plain"
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+
+
+        [Test]
+        public void addBageltest2() 
+        {
+            //testing if the basket is full
+            bool message = _basket.addBagel("Chilli");
+            Assert.That(message, Is.EqualTo("The basket is full"));
+
+        }
+
+
+
+        [Test]
+        public void removeTest()
+        {
+            //testing if an item can be removed
+            _basket.addBagel("Plain");
+            _basket.addBagel("Chilli");
+            bool result = _basket.removeBagel("Plain");
+            Assert.That(result, Is.EqualTo(true));
+
+
+            //testing that the item is not in the list anymore
+            List<string> expected = new List<string> {"Chilli"};
+
+            //Assert tha list does not contains "Plain"
+            Assert.That(result, Is.EqualTo(expected));
+
+        }
+
+        [Test]
+        public void removeTest2()
+        {
+            //testing if the basket is full
+            _basket.addBagel("Plain");
+            bool message = _basket.removeBagel("Chilli");
+
+            Assert.That(message, Is.EqualTo("The basket is full"));
+        }
+
+
+
+        [Test]
+        //testing if the capacity of the basket will be changed as expected
+        public void changeCapacityTest()
+        {
+            int result = _basket.changeCapacity(2);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+
+    }
+}
