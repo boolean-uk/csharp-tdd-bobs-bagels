82c3a6d6f15f930ca03583245ab6f948d5fefc31
kristianverduin <kristianverduin@gmail.com>
2024-01-15 13:05:11 +0100

    Added Add Method

--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -8,5 +8,30 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Core
     {
+        private List<string> _bagels;
+        int _nrBagels;
+        int _capacity;
+
+        public Core()
+        {
+            _bagels = new List<string>();
+            _nrBagels = 0;
+            _capacity = 5;
+        }
+
+        public List<string> Bagels { get { return _bagels; } }
+
+        public void Add(string bagel)
+        {
+            if (_nrBagels != _capacity)
+            {
+                Bagels.Add(bagel);
+                _nrBagels++;
+            }
+            else
+            {
+                throw new Exception("Basket is full");
+            }
+        }
     }
 }
