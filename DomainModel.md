85e1f05bf7063f77c62a877940ed6f9a7f709eff
Jens Arvid Forgård <jensarvid99@gmail.com>
2024-01-16 08:39:48 +0100

    Testsmade

---
+++ DomainModel.md
@@ -0,0 +1,7 @@
+| Classes         | Methods                         | Scenario						| Outputs				|
+|-----------------|---------------------------------|-------------------------------|-----------------------|
+| `AddBagel`      | Add.Bagel						| Adds a bagel to cart			|Bagel added			|
+| `RemoveBagel`   | Remove.Bagel					| Removes Bagel from cart		|Bagel removed			|
+| `BasketCapacity`| if Basket>Capacity				| Adds Bagel to full basket		|Basket is full			|
+| `IncreaseBasket`| Capcity.increase				| Increase basket size		 	|Basket size increased	|
+| `RemoveNotFound`|Remove.Bagel	!					| Removes nonexisting Bagel 	|bagel not in basket	|
--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -1,15 +1,32 @@
-namespace csharp_tdd_bobs_bagels.tests;
+using System.Threading.Tasks.Sources;
+using tdd_bobs_bagels.CSharp.Main;
 
+
+
+namespace csharp_tdd_bobs_bagels.tests;
+[TestFixture]
 public class Tests
 {
-    [SetUp]
-    public void Setup()
+
+    [Test]
+    public void AddTest()
     {
+        A Core core = new Core();
+
+        core.Add("SuperUltraFancy");
+
+        Assert.That(core.Bagels.Count, Is.EqualTo(1));
+        Assert.That(core.Bagels[0], Is.EqualTo("SuperUltraFancy"));
     }
 
     [Test]
-    public void Test1()
+    public void RemoveTest()
     {
-        Assert.Pass();
+        A Core core = new Core();
+
+        Core.Add("SuperUltraFancy");
+        Core.Remove("SuperUltraFancy");
+
+        Assert.That(core.Bagels.Count, Is.EqualTo(0));
     }
 }
