1ad91b5970738eb3abc365ccfb826f082ccd0db1
Stian Gaustad <stian.gaustad@no.experis.com>
2024-01-16 08:39:18 +0100

    Comments

--- tdd-bobs-bagels.CSharp.Main/Basket.cs
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -17,6 +17,11 @@ namespace tdd_bobs_bagels.CSharp.Main
             _maxBasketSize = 5;
         }
 
+        /// <summary>
+        /// Attempt to add a bagel type to the basket.
+        /// </summary>
+        /// <param name="bagelType"> string - the name of the bageltype to be added </param>
+        /// <returns> bool - True if the bagel was added, otherwise false </returns>
         public bool AddBagelToBasket(string bagelType) 
         {
             if (!HasRoom()) 
@@ -37,11 +42,20 @@ namespace tdd_bobs_bagels.CSharp.Main
             }
         }
 
+        /// <summary>
+        /// Attempt to remove a specific bagel from the basket. 
+        /// </summary>
+        /// <param name="bagelType">string - The name of the bagel to be removed</param>
+        /// <returns>bool - True if the bagel was successfully removed from the basket, false if the bagel could not be removed. </returns>
         public bool RemoveBagelFromBasket(string bagelType) 
         {
             return _basket.Remove(bagelType);
         }
 
+        /// <summary>
+        /// Method that determines if the basket object has room for additional items.
+        /// </summary>
+        /// <returns>bool - True if the basket has room for additional items, false if there is no more room.</returns>
         public bool HasRoom()
         {
             if ((_basket.Count) < _maxBasketSize)
@@ -58,9 +72,8 @@ namespace tdd_bobs_bagels.CSharp.Main
         /// <summary>
         /// Changes the maximum size of the basket. If size is set below current capacity any overflow is deleted.
         /// </summary>
-        /// <param name="newSize"></param>
+        /// <param name="newSize"> int - The new size of the basket. </param>
         /// <returns>The number of items remaining in the basket. For basket increases this value will not change, but it will change for size reduction</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public int SetBasketSize(int newSize) 
         {
             _maxBasketSize = newSize;
