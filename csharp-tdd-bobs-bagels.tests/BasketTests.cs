fac998ef26daf07ea84bec3fa381634d4669134d
Mateusz Pietrzak <matti3301@gmail.com>
2024-01-16 10:55:10 +0100

    Added code to pass 1st test and refactored the test

--- csharp-tdd-bobs-bagels.tests/BasketTests.cs
+++ csharp-tdd-bobs-bagels.tests/BasketTests.cs
@@ -1,4 +1,5 @@
 using tdd_bobs_bagels.CSharp.Main;
+using NUnit.Framework;
 
 namespace csharp_tdd_bobs_bagels.tests;
 
@@ -14,8 +15,15 @@ public class Tests
     {
         Basket basket = new Basket();
 
-        basket.Add("test"); 
+        string result1 = basket.AddBagel(Basket.Bagel.Egg);
+        string result2 = basket.AddBagel(Basket.Bagel.Plain);
+        string result3 = basket.AddBagel(Basket.Bagel.Everything);
+        string result4 = basket.AddBagel(Basket.Bagel.Sesame);
 
-        Assert.AreEqual("test", basket.ToString());
+        Assert.That(result1, Is.EqualTo("Bagel added to the basket."));
+        Assert.That(result2, Is.EqualTo("Bagel added to the basket."));
+        Assert.That(result3, Is.EqualTo("Bagel added to the basket."));
+        Assert.That(result4, Is.EqualTo("Basket is full."));
+        Assert.That(basket.GetBasket().Count, Is.EqualTo(3));
     }
 }
--- tdd-bobs-bagels.CSharp.Main/Basket.cs
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -8,5 +8,27 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Basket
     {
+        public enum Bagel
+        {
+            Plain, Everything, Egg, Sourdough, Blueberry, Montreal, 
+            Cinnamon, Salt, Sesame
+        }
+        private int basketSize = 3;
+        private List<Bagel> basket = new List<Bagel>(); 
+        public List<Bagel> GetBasket() { return basket; }
+
+        public string AddBagel(Bagel bagel)
+        {
+            if (basket.Count < basketSize && Enum.IsDefined(typeof(Bagel), bagel))
+            {
+                basket.Add(bagel);
+                return "Bagel added to the basket.";
+            }
+            else if (basket.Count >= basketSize) 
+            {
+                return "Basket is full.";
+            }
+            else { return "Error";  } 
+        }
     }
 }
