172571db28d68b05dcf314f389afed9ecb7d39ba
Martin Skivenesvåg Johannessen <martin.johannessen@no.experis.com>
2024-01-16 11:22:17 +0100

    Martin S. Johannessen - tests finished

---
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Basket
+    {
+        private List<string> _items;
+
+        public Basket()
+        {
+            _items = new List<string>();
+        }
+
+        public string AddProduct(string cheeseBagel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeCapacity(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetItems()
+        {
+            return _items;
+        }
+
+        public string RemoveProduct(string cheeseBagel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
