879056d5654c79d57cacef95e6c0a3b8acfc83ac
Santhia <santhia.shanmukarasan@gmail.com>
2024-01-16 13:58:52 +0100

    Bagel exercise done

---
+++ csharp-tdd-bobs-bagels.tests/BasketTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using tdd_bobs_bagels.CSharp.Main;
+
+namespace csharp_tdd_bobs_bagels.tests
+{
+    public class BasketTest
+    {
+        [Test]
+        public void AddBagelToBasket()
+        {
+            // Arrange
+            Basket basket = new Basket();
+
+            // Act
+            basket.AddBagel("Blueberry Bagel", 2);
+            basket.AddBagel("Cinnamon Raisin Bagel", 2);
+
+            // Assert
+            Assert.That(basket.Items, Is.Not.Null);
+            Assert.That(basket.Items.ContainsKey("Blueberry Bagel"));
+            Assert.That(basket.Items.ContainsKey("Cinnamon Raisin Bagel"));
+
+        }
+
+        [Test]
+        public void RemoveBagel()
+        {
+            // Arrange
+            Basket basket = new Basket();
+            basket.AddBagel("Blueberry Bagel", 2);
+            basket.AddBagel("Cinnamon Raisin Bagel", 2);
+
+            // Act
+            basket.RemoveBagel("Blueberry Bagel");
+
+            // Assert
+            Assert.That(basket.Items.Count, Is.EqualTo(1), "Basket should contain 1 bagel.");
+            Assert.IsFalse(basket.Items.ContainsKey("Blueberry Bagel"));
+        }
+
+
+        [Test]
+        public void IsBasketFull()
+        {
+            // Arrange
+            Basket basket = new Basket();
+
+            //Act
+            basket.AddBagel("Blueberry Bagel", 3);
+            basket.AddBagel("Cinnamon Raisin Bagel", 4);
+            basket.AddBagel("Chocolate Bagel", 5);
+            basket.AddBagel("Onion Bagel", 2);
+            bool fullBasket = basket.IsBasketFull(basket.Items);
+
+            // Assert
+            Assert.That(fullBasket);
+
+        }
+
+        [Test]
+        public void ChangeBasketCapacity()
+        {
+            //Arrange
+            Basket basket = new Basket();
+
+            // Act
+            bool result = basket.ChangeBasketCapacity(15);
+
+
+            //Assert
+            Assert.IsTrue(result, "Capacity should be changed to 15.");
+        }
+
+        [Test]
+        public void RemoveItemThatDoesntExist()
+        {
+            //Arrange
+            Basket basket = new Basket();
+            basket.AddBagel("Chocolate Bagel", 5);
+            basket.AddBagel("Onion Bagel", 2);
+
+            //Act
+            bool exists1 = basket.RemoveItemThatDoesntExist("Onion Bagel");
+            bool exists2 = basket.RemoveItemThatDoesntExist("Blueberry Bagel");
+
+            //Assert
+            Assert.IsTrue(exists1);
+            Assert.IsFalse(exists2);
+        }
+    }
+}
---
+++ tdd-bobs-bagels.CSharp.Main/Basket.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_bobs_bagels.CSharp.Main
+{
+    public class Basket
+    {
+        public int BasketCapacity { get; set; } = 10;
+
+        public Dictionary<string, int> Items = new Dictionary<string, int>();
+       
+
+        public bool AddBagel(string bagelType, int price)
+        {
+            // Check if the basket is full 
+            if (Items.Count >= BasketCapacity)
+            {
+                return false; // Basket is full
+            } else
+
+            Items.Add(bagelType, price);
+            // Bagel added successfully
+            return true; 
+        }
+
+
+        public bool RemoveBagel(string bagelType)
+        {
+            //check if the basket is empty
+            if (Items.Count == 0)
+            {
+                return false; // Basket is empty
+            }
+            // Find and remove the specified bagel type
+            bool removed = Items.Remove(bagelType);
+
+            // Return true if the bagel is found and removed; otherwise, false
+            return removed;
+        }
+
+        public bool IsBasketFull(Dictionary<string,int> items)
+        {
+            if (Items.Count > BasketCapacity)
+            {
+                return false;
+
+            } else 
+            return true;
+        }
+
+        public bool ChangeBasketCapacity(int newCapacity)
+        {
+            if (newCapacity <= 0 || newCapacity == BasketCapacity)
+            {
+                return false; // Invalid or same capacity
+            }
+
+            BasketCapacity = newCapacity;
+
+            //  If capacity changed successfully
+            return true; 
+        }
+
+        public bool RemoveItemThatDoesntExist (string bagelType)
+        {
+            if (!Items.ContainsKey(bagelType))
+                return false;
+            else
+            {
+                return true;
+            }
+        }
+
+
+    }
+}
