f596c0afbbe9babbb43c450abcc518c4a28decbc
Aleksander Kolsrud <kols203@gmail.com>
2024-01-16 14:38:34 +0100

    Aleksander Kolsrud C#ex3

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -1,15 +1,59 @@
+using NUnit.Framework.Internal;
+using tdd_bobs_bagels.CSharp.Main;
+
 namespace csharp_tdd_bobs_bagels.tests;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
 
-    [Test]
-    public void Test1()
+    public class BagelTests
     {
-        Assert.Pass();
+       
+        
+        [Test]
+        public void AddBagel()
+        {
+            //arrange in SetUp
+            BobsBasket basket = new BobsBasket();
+
+            //act
+            bool expected = basket.addBagel("Bagel", 10);
+
+        //assert
+            Assert.That(expected = true);    
+        }
+
+        [Test]
+        [TestCase("Pineapple", 1, 1)]
+        [TestCase("Pistachio", 5, 0)]
+        [TestCase("Whole wheat", 3, 3)]
+        public void removeBagel(string type, int amount, int expected)
+            {
+                BobsBasket basket = new BobsBasket();
+
+                basket.addBagel("Pineapple", 2);
+                basket.addBagel("Pistachio", 10);//max per basket is 5, returns 0
+                basket.addBagel("Whole wheat", 3);
+
+                basket.removeBagel(type, amount);
+
+                int bagels = basket.roomForMoreBagels();
+    
+                Assert.That(bagels, Is.EqualTo(expected));
+
+            }
+
+        [Test]  
+        public void changeBasketSize()
+        {
+            BobsBasket basket = new BobsBasket();
+
+            basket.changeBasketSize(10);
+
+            int result = basket.maxAmountOfBagels();
+
+            Assert.That(result == 10);
+
+        }
+
+    
+    
     }
-}
--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace tdd_bobs_bagels.CSharp.Main
 {
-    public class Core
+    public class BobsBasket
     {
+
+        private List<string> _BobsBasket = new List<string>();
+        private int _Basketsize = 5;
+
+        public int basketSize { get { return _Basketsize;  } set { _Basketsize = value; } }
+
+
+        public bool addBagel(string type, int amount)
+        {
+            if ((_BobsBasket.Count + amount) > basketSize)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < amount; i++)
+                {
+                    _BobsBasket.Add(type);
+                }
+                return true;
+            }
+        }
+
+        public void removeBagel (string type, int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                _BobsBasket.Remove(type);
+            }
+        }
+
+        public void changeBasketSize(int newSize)
+        {
+            basketSize = newSize;
+        }
+
+        public int roomForMoreBagels()
+        {
+            int current = basketSize - _BobsBasket.Count();
+            return current;
+        }
+
+        public int maxAmountOfBagels()
+        {
+            return basketSize;
+        }
+
     }
 }
